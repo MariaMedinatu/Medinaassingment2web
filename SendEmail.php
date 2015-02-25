589450be91a64dfc7b88042e940844578c03df7e
MariaMedinatu <maria.medina.aiesec@gmail.com>
2015-02-25 22:20:37 +0200

    Update SendEmail.php

--- SendEmail.php
+++ SendEmail.php
@@ -1,3 +1,22 @@
-Thank you so much!!
+<html>
+<body>
 
--Esensorics team
+  <?php
+    if (empty ($_POST["mailsubject"]) ){
+       $mailsubject=" " ;
+    }
+
+    if (empty ($_POST["mailbody"]) ) {
+       $mailbody=" " ; 
+    }
+
+    $result = mail ("maria.medina.aiesec@gmail.com", $_POST["mailsubject"], $_POST["mailbody"]) ;
+
+    if ($result) {
+       echo "Email sent successfully!" ;
+    }else{
+       echo "Email could not be sent." ;
+    }
+?>
+</body>
+</html> 
